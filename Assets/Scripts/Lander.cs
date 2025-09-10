2c5ba06aebd3fa68b7d70c0565040bd8a57a2f39
Danh <cogdanh2k3@gmail.com>
2025-09-10 10:30:15 +0700

    background, terrain, landing detection

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -26,4 +26,24 @@ public class Lander : MonoBehaviour
             landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        float softLandingVelocityMagnitude = 4f;
+        if(collision2D.relativeVelocity.magnitude > softLandingVelocityMagnitude)
+        {
+            Debug.Log("Landed too hard!");
+            return;
+        }
+
+        float dotProduct = Vector2.Dot(Vector2.up, transform.up);
+        float minDotProduct = 0.9f;
+
+        if(dotProduct < minDotProduct)
+        {
+            Debug.Log("Landed on a too steep angle!");
+            return;
+        }
+        Debug.Log("Successful landed!");
+    }
 }
